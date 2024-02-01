715202d06dc8ce7226d1afea7b520e83fca60448
Bastien Sauvat <bastien.sauvat@etudiant.univ-rennes1.fr>
2024-02-01 13:33:22 +0100

    Initial commit

---
+++ src/test/java/fr/esir/sr/SRproject/SRprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esir.sr.SRproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SRprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

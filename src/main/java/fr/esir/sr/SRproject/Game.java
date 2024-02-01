2df2fcca7c8034087f32ebaddc19ca8557968290
Legrand Quentin <quentin.legrand@etudiant.univ-rennes1.fr>
2024-02-01 14:15:51 +0100

    Initial commit

---
+++ src/main/java/fr/esir/sr/SRproject/Game.java
@@ -0,0 +1,4 @@
+package fr.esir.sr.SRproject;
+
+public interface Game extends java.rmi.Remote{
+}
---
+++ src/main/java/fr/esir/sr/SRproject/GameServer.java
@@ -0,0 +1,20 @@
+package fr.esir.sr.SRproject;
+import java.rmi.Naming;	//Import naming classes to bind to rmiregistry
+
+public class GameServer {
+
+    public GameServer() {
+
+        try {
+            GameImpl g = new GameImpl();
+            Naming.rebind("rmi://localhost/CalculatorService", c);
+        }
+        catch (Exception e) {
+            System.out.println("Server Error: " + e);
+        }
+    }
+
+    public static void main(String args[]) {
+        new GameServer();
+    }
+}
